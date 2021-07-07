285091a58ca7dcff3f3fb5abbf2397f74384a19c
pbrmajhar <pbrmajhar@gmail.com>
2021-07-07 14:37:10 +0600

    '25'

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
 require("dotenv").config();
-const mongoose = require("./model");
+require("./model");
 const app = express();
 
 // import routes
---
+++ src/model/user.model.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    name: String,
+    email: {
+      type: String,
+      require: true,
+      index: true,
+      unique: true,
+    },
+    role: {
+      type: String,
+      default: "subscriber",
+    },
+    cart: {
+      type: Array,
+      default: [],
+    },
+    address: String,
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model('User', userSchema)
