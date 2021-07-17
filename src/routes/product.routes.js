e180d39fa64b77091a35644440819d839d303955
pbrmajhar <pbrmajhar@gmail.com>
2021-07-17 22:59:59 +0600

    Product create route

--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -3,10 +3,12 @@ const { authCheck, adminCheck } = require("../middleware/auth.middleware");
 const Product = require("../model/product.model");
 const slugify = require("slugify");
 
-router.post("/create", (req, res) => {
+router.post("/create", authCheck, adminCheck, async (req, res) => {
   try {
-      
     console.log(req.body);
+    req.body.slug = slugify(req.body.title);
+    const product = await new Product(req.body).save();
+    res.send(product);
   } catch (error) {
     console.log(error);
     res.status(400).send("admin admin");
