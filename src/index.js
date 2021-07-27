9cb41b6370b0c8e68cca0dc90bb9b36715226b33
pbrmajhar <pbrmajhar@gmail.com>
2021-07-27 13:47:06 +0600

    working on file upload

--- src/index.js
+++ src/index.js
@@ -9,8 +9,9 @@ const app = express();
 // import routes
 const authRoutes = require("./routes/auth.routes");
 const categoryRoutes = require("./routes/category.routes");
-const subcategory = require('./routes/subcategory.routes')
-const product = require('./routes/product.routes')
+const subcategory = require("./routes/subcategory.routes");
+const product = require("./routes/product.routes");
+const upload = require("./routes/file.routes");
 
 const PORT = process.env.PORT || 4000;
 
@@ -23,6 +24,7 @@ app.use("/api", authRoutes);
 app.use("/api/cat", categoryRoutes);
 app.use("/api/sub/cat", subcategory);
 app.use("/api/product", product);
+app.use("/api/", upload);
 
 app.listen(PORT, () => {
   console.log(`express server is running on port ${PORT}`);
